4fd7dea8b9309b9c48c9c40c78793d8687f28fd6
Robin <robin.janke@gmx.de>
2024-05-19 19:58:00 +0200

    Changed Admin-Feature-Icon to work without Taiga UI

--- apps/singularity-client/src/app/shared/components/admin-feature-icon/admin-feature-icon.component.ts
+++ apps/singularity-client/src/app/shared/components/admin-feature-icon/admin-feature-icon.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'singularity-admin-feature-icon',
   templateUrl: './admin-feature-icon.component.html',
-  styleUrls: ['./admin-feature-icon.component.css']
+  styleUrls: ['./admin-feature-icon.component.scss']
 })
 export class AdminFeatureIconComponent {
   @Input() size: 'small' | 'large' = 'small';
