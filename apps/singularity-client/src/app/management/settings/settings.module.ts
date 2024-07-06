2fe4b095e6315fd79c350ffaca6c7fca4b72f3dc
Robin <robin.janke@gmx.de>
2024-07-06 21:01:01 +0200

    removed Taiga UI in several places

--- apps/singularity-client/src/app/management/settings/settings.module.ts
+++ apps/singularity-client/src/app/management/settings/settings.module.ts
@@ -2,27 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
-import {
-  TuiInputCountModule,
-  TuiIslandModule,
-  TuiProgressModule,
-  TuiRadioBlockModule,
-  TuiSelectModule, TuiSliderModule
-} from '@taiga-ui/kit';
-import {
-  TuiButtonModule,
-  TuiDataListModule,
-  TuiGroupModule,
-  TuiLabelModule,
-  TuiTextfieldControllerModule
-} from '@taiga-ui/core';
 import { FormsModule } from '@angular/forms';
 import { TranslocoModule } from '@ngneat/transloco';
 import { SingularityUiModule } from '@singularity/ui';
 
 @NgModule({
   declarations: [SettingsComponent],
-  imports: [CommonModule, SettingsRoutingModule, TuiIslandModule, TuiButtonModule, TuiInputCountModule, TuiLabelModule, FormsModule, TuiSelectModule, TuiTextfieldControllerModule, TuiDataListModule, TuiGroupModule, TuiRadioBlockModule, TuiProgressModule, TranslocoModule, TuiSliderModule, SingularityUiModule],
-  providers: [],
+  imports: [CommonModule, SettingsRoutingModule, FormsModule, TranslocoModule, SingularityUiModule],
+  providers: []
 })
-export class SettingsModule {}
+export class SettingsModule {
+}
