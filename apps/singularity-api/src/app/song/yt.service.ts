a33c5a40912d09e77618af32bbda5f3c28115258
Robin <robin.janke@gmx.de>
2024-12-15 12:29:02 +0100

    Added Proxy Support

--- apps/singularity-api/src/app/song/yt.service.ts
+++ apps/singularity-api/src/app/song/yt.service.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { VideoInfo } from '@singularity/api-interfaces';
 import * as ytdl from '@distube/ytdl-core';
 import { SongFile } from './interfaces/song-file';
+import { ConfigService } from '@nestjs/config';
+import { Agent } from '@distube/ytdl-core';
 
 @Injectable()
 export class YtService {
 
+  private readonly agent: Agent;
+
+  constructor(configService: ConfigService) {
+    const proxy = configService.get('YOUTUBE_PROXY');
+
+    if(proxy) {
+     this.agent = ytdl.createProxyAgent({ uri: proxy });
+    } else {
+      this.agent = ytdl.createAgent();
+    }
+  }
+
   public async getInfo(id: string): Promise<VideoInfo> {
-    const ytdlVideoInfo = await ytdl.getBasicInfo(id);
+    const ytdlVideoInfo = await ytdl.getBasicInfo(id, { agent: this.agent });
     const videoInfo = new VideoInfo();
 
     videoInfo.id = id;
@@ -31,7 +45,7 @@ export class YtService {
   }
 
   public async downloadVideo(id: string): Promise<SongFile> {
-    const info = await ytdl.getInfo(id);
+    const info = await ytdl.getInfo(id, { agent: this.agent });
     const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo', filter: 'videoonly' });
 
     const buffer = await this.download(info, format);
@@ -40,7 +54,7 @@ export class YtService {
   }
 
   public async downloadAudo(id: string): Promise<SongFile> {
-    const info = await ytdl.getInfo(id);
+    const info = await ytdl.getInfo(id, { agent: this.agent });
     const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
 
     const buffer = await this.download(info, format);
@@ -50,7 +64,7 @@ export class YtService {
 
   private async download(info: ytdl.videoInfo, format: ytdl.videoFormat): Promise<Buffer> {
     return new Promise((resolve, reject) => {
-      const stream = ytdl(info.videoDetails.video_url, { format: format })
+      const stream = ytdl(info.videoDetails.video_url, { format: format, agent: this.agent })
       const chunks: Buffer[] = [];
       stream.on('data', (chunk: Buffer) => chunks.push(chunk));
       stream.on('end', () => resolve(Buffer.concat(chunks)));
